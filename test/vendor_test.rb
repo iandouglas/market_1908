c968cf1c88c0cbe6c6a58bd72d978ff407791711
ian douglas <ian.douglas@iandouglas.com>
2019-09-11 13:17:56 -0600

    i1 and i2 done, 15 minute commit

---
+++ test/vendor_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_initialize
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+    assert_equal ({}), @vendor.inventory
+  end
+
+  def test_check_stock_and_stock
+    assert_equal 0, @vendor.check_stock("Peaches")
+    @vendor.stock("Peaches", 30)
+    assert_equal 30, @vendor.check_stock("Peaches")
+    @vendor.stock("Peaches", 25)
+    assert_equal 55, @vendor.check_stock("Peaches")
+  end
+
+  def test_inventory_hash
+    @vendor.stock("Peaches", 55)
+    @vendor.stock("Tomatoes", 12)
+
+    expected = {"Peaches"=>55, "Tomatoes"=>12}
+    assert expected, @vendor.inventory
+  end
+
+
+end
